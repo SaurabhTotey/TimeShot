bbe5fad726758fb9357dfd5d8df31c93dab5361b
Saurabh Totey <SaurabhTotey@gmail.com>
2018-02-11 22:13:53 -0700

    Started implementing primitive ball swiping

--- app/src/main/java/com/saurabhtotey/timeshot/Game.kt
+++ app/src/main/java/com/saurabhtotey/timeshot/Game.kt
@@ -1,47 +1,79 @@
 package com.saurabhtotey.timeshot
 
+import android.graphics.Point
+import android.graphics.Rect
 import android.os.Bundle
 import android.support.v4.view.GestureDetectorCompat
 import android.support.wearable.activity.WearableActivity
-import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.widget.ImageView
+import kotlin.math.roundToInt
 
 /**
  * The actual game that is run
  */
 class Game : WearableActivity() {
 
+    private lateinit var ball: ImageView
     private lateinit var gameState: GameState
     private lateinit var gestureDetector: GestureDetectorCompat
 
     /**
-     * When the game is created, TODO: do stuff
+     * When the game is created, initializes the gamestate and starts running the game and updating graphics on a separate thread
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        this.gameState = GameState(GameMode.values().firstOrNull { it.label == intent.getStringExtra("mode") }!!)
-        this.gestureDetector = GestureDetectorCompat(this, SwipeHandler(this.gameState))
+        this.ball = findViewById(R.id.ball)
+        val displaySize = Point().also { windowManager.defaultDisplay.getSize(it) }
+        this.gameState = GameState(GameMode.values().firstOrNull { it.label == intent.getStringExtra("mode") }!!, displaySize.x.toFloat() / 2, displaySize.y.toFloat() / 2)
+        this.gestureDetector = GestureDetectorCompat(this, SwipeHandler(this.gameState, this.ball))
+        Thread {
+            while (!this.gameState.isFinished) {
+                this.gameState.update()
+                this.updateBallPos()
+                Thread.sleep(gameSpeed)
+            }
+        }.start()
+    }
+
+    /**
+     * Sets the ball's position to what is specified by in gameState
+     */
+    private fun updateBallPos() {
+        runOnUiThread {
+            this.ball.x = this.gameState.currentX - ball.width / 2
+            this.ball.y = this.gameState.currentY - ball.height / 2
+        }
     }
 
     /**
      * If a touch is detected, ensures that the gestureDetector can handle the motion
      */
     override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
-        return this.gestureDetector.onTouchEvent(event)
+        if (this.gestureDetector.onTouchEvent(event)) {
+            return true
+        }
+        return super.dispatchTouchEvent(event)
     }
 
     /**
      * An internal class that handles taking in touch events
      */
-    class SwipeHandler(val gameState: GameState): GestureDetector.SimpleOnGestureListener() {
+    class SwipeHandler(var gameState: GameState, val ball: ImageView) : GestureDetector.SimpleOnGestureListener() {
 
         /**
          * When a fling happens in the center, the ball is shot towards that direction
          */
         override fun onFling(p0: MotionEvent?, p1: MotionEvent?, xVelocity: Float, yVelocity: Float): Boolean {
-            Log.d("TRACE", "User has made a fling gesture from (${p0!!.x}, ${p0.y}) to (${p1!!.x}, ${p1.y})")
+            p0!!
+            p1!!
+            val bounds = Rect().also { this.ball.getHitRect(it) }
+            if (!bounds.contains(p0.x.roundToInt(), p0.y.roundToInt())) {
+                return false
+            }
+            gameState.swipe(xVelocity, yVelocity)
             return true
         }
 
--- app/src/main/java/com/saurabhtotey/timeshot/GameState.kt
+++ app/src/main/java/com/saurabhtotey/timeshot/GameState.kt
@@ -1,5 +1,8 @@
 package com.saurabhtotey.timeshot
 
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 /**
  * A set of enums that represent the different gamemodes which affects gameplay and game type
  */
@@ -7,11 +10,38 @@ enum class GameMode(val label: String) {
     RANDOM("RANDOM"), CURRENT("CURRENT")
 }
 
+const val gameSpeed: Long = 1000 / 20 //The delay between ticks for the game in milliseconds
+
 /**
  * A class that represents how the game is currently
  */
-class GameState(val gameMode: GameMode) {
+class GameState(val gameMode: GameMode, val initialX: Float, val initialY: Float) {
 
+    var currentX: Float = initialX
+    var currentY: Float = initialY
+    val speed = 1
+    private var velocityX: Float = 0.toFloat()
+    private var velocityY: Float = 0.toFloat()
     var isFinished = false
+
+    /**
+     * Performs one game tick and moves the ball
+     */
+    fun update() {
+        if (this.isFinished) {
+            return
+        }
+        this.currentX += this.velocityX
+        this.currentY += this.velocityY
+    }
+
+    /**
+     * Receives component velocities and sets the ball's velocity as such
+     */
+    fun swipe(velocityX: Float, velocityY: Float) {
+        val magnitude = sqrt(velocityY.pow(2) + velocityX.pow(2))
+        this.velocityX = this.speed * velocityX / magnitude
+        this.velocityY = this.speed * velocityY / magnitude
+    }
 
 }
